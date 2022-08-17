3f3e42fc92e5b6d2d0b689e364a28bb4b644aede
stella <stellavir11@gmail.com>
2022-08-17 21:19:15 +0300

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const http = require('http');
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Heroku world!");
+  response.write("Finally! Hello World!");
   response.end();
 }).listen(process.env.PORT);
+
+but you can't add html here
