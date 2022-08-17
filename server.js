2679ad1ba1e74699f25a5fd4c98d63df4d8f6a75
stella <stellavir11@gmail.com>
2022-08-17 21:26:04 +0300

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 const http = require('http');
 
 http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Finally! Hello World!");
-  response.end();
+  // response.writeHead(200, {"Content-Type": "text/plain"});
+  // response.write("Finally! Hello World!");
+  // response.end();
+  response.sendFile(__dirname+'/css/styles.css');
 }).listen(process.env.PORT);
