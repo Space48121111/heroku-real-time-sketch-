db33efdf2502b03b2f79017317aae65c27f5e006
stella <stellavir11@gmail.com>
2022-08-18 18:46:05 +0300

    added express and socket.io again

--- server.js
+++ server.js
@@ -1,9 +1,27 @@
-
+const express = require('express');
+const app = express();
 const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
-http.createServer(function(request, response) {
-  // response.writeHead(200, {"Content-Type": "text/plain"});
-  // response.write("Finally! Hello World!");
-  // response.end();
-  response.sendFile(__dirname+'/css/styles.css');
-}).listen(process.env.PORT);
+// http.createServer(function(request, response) {
+//   // response.writeHead(200, {"Content-Type": "text/plain"});
+//   // response.write("Finally! Hello World!");
+//   // response.end();
+//   response.sendFile(__dirname+'/css/styles.css');
+// }).listen(process.env.PORT);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('*/css/styles.css', (req, res) => {
+  res.sendFile(__dirname + '/css/styles.css');
+});
+
+
+
+server.listen(process.env.PORT, () => {
+  console.log('Listening on process.env.PORT');
+});
