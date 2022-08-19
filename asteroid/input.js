c02f96129b59dfbc972b0b84eefbcd439d73e95c
Stella <stellavir11@gmail.com>
2022-08-19 23:17:23 +0300

    Update input.js

--- asteroid/input.js
+++ asteroid/input.js
@@ -22,6 +22,7 @@ document.addEventListener('keydown', (event) => {
 	{
 		users[i].upPressed = true;
 		socket.emit("up", true);
+		console.log("Client side up pressed!");
 	}
 	else if (event.key === "d") 
 	{
