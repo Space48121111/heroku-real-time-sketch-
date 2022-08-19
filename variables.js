a3483e94748094e4b8ccde313cc277fd20e8e72d
Stella <stellavir11@gmail.com>
2022-08-19 20:31:37 +0300

    Update variables.js

--- variables.js
+++ variables.js
@@ -7,24 +7,4 @@ var yAxis = [-10, -10, 10, 10];
 var engineX = [-5, 5];
 var engineY = [17, 17];
 var time = 0;
-var laserX = [];
-var laserY = [];
-var laserAngle = [];
-var laserVelocity = 10;
-var laserLength = 5;
-var isSpacePressed = false;
-var laserFreq = 2;
-var laserTimer = 0
-var asteroids = [];
-var asteroidRadiusMin = 8;
-var asteroidRadiusMax = 10;
-var asteroidsNum = 18;
-var asteroidRotationSpeed = 3;
-var asteroidMovingSpeed = 0.5;
-// new asteroid spawning
-var wanderingStarsDelay = 20;
-var cometsTimer = 0;
-var trackingRecord = 0;
-var bigBang = [];
-var bigBangMaxRadius = 11;
-var spikesRadius = 5;
+
