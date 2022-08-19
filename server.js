5507f6fd0248329cbf4bbafe0016cda7f4907bd7
Stella <stellavir11@gmail.com>
2022-08-19 20:39:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ var convo = [];
 const fs1 = require('fs');
 const fs = require('fs').promises;
 
+const v = require("./serverVariables.js");
+var sp = require("./serverPlayer.js");
+
 function loadMessages()
 {
 	var doesExist = fs1.existsSync(__dirname + "/messages.json");
@@ -47,18 +50,19 @@ function drawLine(startX, startY, x, y) {
   io.emit('pixels', pixels);
 }
 
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('*/index.js', (req, res) => {
-  res.sendFile(__dirname + '/index.js');
-});
+var allowedFiles = ["/Asteroid.html", "/index.js", "/css/styles.css", "/input.js", "/clientPlayer.js",  "/utilFuncs.js"]; 
 
-app.get('*/css/styles.css', (req, res) => {
-  res.sendFile(__dirname + '/css/styles.css');
-});
+//Add all the allowed files
+for (var i=0; i<allowedFiles.length; i++)
+{
+	app.get('*' + allowedFiles[i], (req, res) => {
+  	res.sendFile(__dirname + allowedFiles[i]);
+	});	
+}
 
 function receiveMessage(message, index, id)
 {
@@ -74,18 +78,58 @@ function receiveMessage(message, index, id)
 	transmitMessages();
 }
 
+
+function addPlayer(id)
+{
+	var newPlayer =
+	{
+		x: 100,
+		y: 100,
+		angle: 0,
+		velocityX: 0,
+		velocityY: 0,
+		upPressed: false,
+		rightPressed: false,
+		downPressed: false,
+		leftPressed: false,
+		id: id
+	};
+	
+	v.users.push(newPlayer);
+}
+
+function removePlayer(id)
+{
+	for (var i=v.users.length-1; i>=0; i--)
+	{
+		if (v.users[i].id == id)
+		{
+				v.users.splice(i, 1);
+				break;
+		}
+	}
+}
+
+function findPlayer(id)
+{
+	for (var i=v.users.length-1; i>=0; i--)
+	{
+		if (v.users[i].id == id)
+		{
+			return i;
+		}
+	}
+	
+	return -1;
+}
+
 io.on('connection', (socket) => {
   console.log('A user has connected.');
   io.emit('pixels', pixels);
   // when a new user enters, it will join the chat.
   transmitMessages();
-  // socket.on('draw', (message) => {
-  //   var pos = message.split(':'); // x+':'+y divide x y into an array [x, y]
-  //   var intX = parseInt(pos[0]);
-  //   var intY = parseInt(pos[1]);
-  //   drawPixels(intX, intY);
-  // })
-  // message: startX+":"+startY+':'+x+':'+y
+  
+  addPlayer(socket.id);
 
   socket.on('chitChat1', (message) => {
 	receiveMessage(message, 1, socket.id);
@@ -108,8 +152,76 @@ io.on('connection', (socket) => {
   });
   socket.on('disconnect', () => {
     console.log('A user has disconnected.');
+    removePlayer(socket.id);
   });
+	
+	socket.on('up', (msg) => 
+	{
+		var i = findPlayer(socket.id);
+		if (i != -1)
+		{
+			v.users[i].upPressed = msg;
+		}
+	});
+
+	socket.on('down', (msg) => 
+	{
+		var i = findPlayer(socket.id);
+		if (i != -1)
+		{
+			v.users[i].downPressed = msg;
+		}
+	});	
+
+	socket.on('right', (msg) => 
+	{
+		var i = findPlayer(socket.id);
+		if (i != -1)
+		{
+			v.users[i].rightPressed = msg;
+		}
+	});	
+
+	socket.on('left', (msg) => 
+	{
+		var i = findPlayer(socket.id);
+		if (i != -1)
+		{
+			v.users[i].leftPressed = msg;
+		}
+	});			
 });
+
+
+//
+function serverFrame()
+{
+	for (var i=0; i<v.users.length; i++)
+	{
+			sp.updateMovement(i);
+			sp.updateRotation(i);
+	}
+	
+	var outUsers = [];
+	for (var i=0; i<v.users.length; i++)
+	{
+		var outUser =
+		{
+			x: v.users[i].x,
+			y: v.users[i].y,
+			angle: v.users[i].angle,
+			id: v.users[i].id
+		};
+
+		outUsers.push(outUser);
+	}
+	
+	io.emit("users", outUsers);
+}
+
+setInterval(function(){
+  serverFrame();
+}, 50);
 
 server.listen(process.env.PORT, () => {
   console.log('Listening on ' + process.env.PORT);
