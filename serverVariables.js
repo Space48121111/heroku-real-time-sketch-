2caac8fafb56b2b519986278a553d9d30f134105
Stella <stellavir11@gmail.com>
2022-08-19 20:29:08 +0300

    Create serverVariables.js

---
+++ serverVariables.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+	//
+	users: [],
+	revolutionSpeed: 0.25,
+	deAcceleration: 0.8,
+	maxSpeed: 50,
+	acceleration: 3,
+	canvasWidth: 450,
+	canvasHeight: 300,
+	shipRadius: 10
+};
