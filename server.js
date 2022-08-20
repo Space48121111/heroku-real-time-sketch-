080386c5caa49d63be842794f76a84525cd4ea0d
Stella <stellavir11@gmail.com>
2022-08-20 17:29:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-var allowedFiles = ["/Asteroid.html", "/index.js", "/variables.js", "/css/styles.css", "/input.js", "/clientPlayer.js",  "/utilFuncs.js", "/background.webp"]; 
+var allowedFiles = ["/Asteroid.html", "/index.js", "/variables.js", "/css/styles.css", "/input.js", "/clientPlayer.js",  "/utilFuncs.js", "/background.webp", "/messages.js"]; 
 
 function FindFile(req)
 {
