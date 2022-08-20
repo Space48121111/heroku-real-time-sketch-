a7b04440bcbb28f6af0906468a3fea3b9d5a7fdd
Stella <stellavir11@gmail.com>
2022-08-20 17:29:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,40 +8,6 @@
       var startY;
       var isDrawing = false;
 
-      function enter1() {
-        var chitChat = document.getElementById('messagebox1').value;
-		if (chitChat.length > 0)
-		{
-			socket.emit('chitChat1', chitChat);
-			document.getElementById('messagebox1').value = '';
-		}
-      }
-	  
-      function enter2() {
-        var chitChat = document.getElementById('messagebox2').value;
-		if (chitChat.length > 0)
-		{
-			socket.emit('chitChat2', chitChat);
-			document.getElementById('messagebox2').value = '';
-		}
-      }	  
-	  
-	  function updateMessages(div, convo, index)
-	  {
-		div.innerHTML = "<ul>";
-		
-		for (var i=0; i<convo.length; i++)
-		{
-			if (convo[i].index == index)
-			{
-				div.innerHTML += "<li>" + convo[i].message + "</li>";
-			}
-		}
-		
-		div.innerHTML += "</ul>";
-		div.scrollTop = div.scrollHeight;
-	  }
-
       socket.on('messages', function(convo) 
 	  {
 		var div1 = document.getElementById("messages1");	
