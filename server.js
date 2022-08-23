4541bbc693e5d457cabf49a7fe49a56ca5b7ad61
Stella <stellavir11@gmail.com>
2022-08-23 20:28:49 +0300

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,42 @@ var isChecked = [];
 const { MongoClient } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 
+// define the first route
+app.get("/api/movie", async function (req, res) {
+  const client = new MongoClient(uri, { useUnifiedTopology: true });
+  
+  try {
+    await client.connect();
+
+    const database = client.db('sample_mflix');
+    const collection = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { genres: "Comedy", poster: { $exists: true } };
+    const cursor = await collection.aggregate([
+      { $match: query },
+      { $sample: { size: 1 } },
+      { $project: 
+        {
+          title: 1,
+          fullplot: 1,
+          poster: 1
+        }
+      }
+    ]);
+
+    const movie = await cursor.next();
+
+    return res.json(movie);
+  } catch(err) {
+    console.log(err);
+  }
+  finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+});
+
 function loadMessages()
 {
 	var doesExist = fs1.existsSync(__dirname + "/messages.json");
