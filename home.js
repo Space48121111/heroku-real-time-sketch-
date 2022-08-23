651dbb16ae4c22af5f9ba889460e80b5811af05e
Stella <stellavir11@gmail.com>
2022-08-23 20:31:17 +0300

    Update home.js

--- home.js
+++ home.js
@@ -87,13 +87,9 @@ function testMongodb() {
   fetch("/api/movie")
     .then((response) => response.json())
     .then((data) => {
-      const detailsElement = document.getElementById("movie");
-
       console.log(data.poster);
       console.log(data.title);
       console.log(data.fullplot);
-
-      detailsElement.style.visibility = "visible";
     });
 }
 
